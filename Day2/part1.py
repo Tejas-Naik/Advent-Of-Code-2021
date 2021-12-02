e02c6d15318543dd3d594812ce194ac1e8ee6977
Tejas-Naik <rntejas2005@gmail.com>
2021-12-02 14:19:34 +0530

    Added day 2 part1 and part2

--- Day2/part1.py
+++ Day2/part1.py
@@ -8,11 +8,12 @@ for inp in content:
     if 'forward' in inp:
         splitted_inp = inp.split()
         horizontal += int(splitted_inp[-1])
+        depth += aim * int(splitted_inp[-1])
     elif 'down' in inp:
         splitted_inp = inp.split()
-        depth += int(splitted_inp[-1])
+        aim += int(splitted_inp[-1])
     elif 'up' in inp:
         splitted_inp = inp.split()
-        depth -= int(splitted_inp[-1])
+        aim -= int(splitted_inp[-1])
 
 print(horizontal * depth)
