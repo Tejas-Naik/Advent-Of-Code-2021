d55118cddf68447efb10a670094a257c5cbcb83f
Tejas-Naik <rntejas2005@gmail.com>
2021-12-02 14:14:26 +0530

    Day2 part1

---
+++ Day2/par1.py
@@ -0,0 +1,19 @@
+with open('input.txt', 'r') as file:
+    content = file.readlines()
+
+horizontal = 0
+depth = 0
+
+for inp in content:
+    if 'forward' in inp:
+        splitted_inp = inp.split()
+        horizontal += int(splitted_inp[-1])
+    elif 'down' in inp:
+        splitted_inp = inp.split()
+        depth += int(splitted_inp[-1])
+    elif 'up' in inp:
+        splitted_inp = inp.split()
+        depth -= int(splitted_inp[-1])
+
+
+print(horizontal * depth)
